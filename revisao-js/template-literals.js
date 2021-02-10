17a4ba8985edf822bc11f741218b9aef330f10b7
Guilherme1993 <guinasc7@gmail.com>
2021-02-10 01:39:47 +0000

    (09/02) Início da revisão de JS

---
+++ revisao-js/template-literals.js
@@ -0,0 +1,8 @@
+let nome = "João"
+let idade = 26
+
+console.log('Meu nome é ' + nome + ' e minha idade é ' + idade + '.')
+
+console.log(`Meu nome é ${nome} e minha idade é ${idade}.`)
+console.log(`Meu nome é ${nome.toUpperCase()} e minha idade é ${idade}.`)
+console.log(`7 vezes 6 é igual a ${7*6}.`)
